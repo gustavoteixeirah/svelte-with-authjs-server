7881a74dab88367075dfc178c502dc118c48f8b0
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-01-07 03:08:34 -0300

    saving progress

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ORIGIN=http://localhost:5173 NODE_ENV=development APP_ENV=local node  ./src/server.js
-# svelte-with-authjs-server
+
+
+https://opentelemetry.io/docs/instrumentation/js/getting-started/nodejs/
+
+
+https://opentelemetry.io/docs/instrumentation/js/exporters/
+
+
+docker run --rm \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:latest
--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,19 +1,23 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
-import {
-    getNodeAutoInstrumentations,
-} from '@opentelemetry/auto-instrumentations-node';
-import {
-    PeriodicExportingMetricReader,
-    ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
+/*instrumentation.ts*/
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
-const sdk = new NodeSDK({
-    traceExporter: new ConsoleSpanExporter(),
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter: new OTLPTraceExporter({
+        // optional - default url is http://localhost:4318/v1/traces
+        url: 'http://localhost:4318/v1/traces',
+        // optional - collection of custom headers to be sent with each request, empty by default
+        headers: {},
+    }),
     metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
+        exporter: new OTLPMetricExporter({
+            url: 'http://localhost:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+            headers: {}, // an optional object containing custom headers to be sent with each request
+        }),
     }),
     instrumentations: [getNodeAutoInstrumentations()],
 });
-
 sdk.start();
